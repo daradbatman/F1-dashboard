50a19969d8fd8331b167714507b9be85c62e1c51
Khan0622 <khan0622@umn.edu>
2025-10-19 09:35:02 -0500

    re order function calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ export default async function Home() {
   const standings = await f1Service.getCurrentDriverStandings();
   const constructorStandings = await f1Service.getCurrentConstructorStandings();
   const nextRaceSchedule = await f1Service.getNextRaceSchedule();
+  const previousRaceResponse = await f1Service.getPreviousRaceResult()
 
   const topDriverStanding = standings?.drivers_championship[0];
   const topConstructorStanding = constructorStandings?.constructors_championship[0];
   const nextRace = nextRaceSchedule?.race?.[0];
-  const previousRace = await f1Service.getPreviousRaceResult().then((data) => data?.races);
-  console.log(previousRace)
+  const previousRace = previousRaceResponse?.races
 
   return (
     <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen mt-0 pt-2 pb-12 px-4 gap-0 sm:pt-4 sm:px-8 font-[family-name:var(--font-geist-sans)]">
